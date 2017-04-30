00cadf94bd3f2c29ea7b1981edd21730d1c11685
André Markwalder <andre.markwalder@gmail.com>
2017-04-30 23:01:45 +0200

    Introduction of working directory

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+if [ -z "${WORKDIR}" ]; then
+  echo "ABORT: env WORKDIR is missing"
+  exit 1
+fi
+
 if [ -z "${GITHUB_USER}" ]; then
   echo "ABORT: env GITHUB_USER is missing"
   exit 1
@@ -16,25 +21,32 @@ if [ -z "${BUILD_NO}" ]; then
   exit 1
 fi
 
-WORKDIR=/tmp
-BUILD_DEST=/builds/${BUILD_NO}
-
-mkdir -p ${BUILD_DEST}
+WORKDIR=${WORKDIR:-/tmp}
+BUILDS_DIR=${WORKDIR}/builds
+SOURCE_DIR=${WORKDIR}/source
 
 echo "npm     : " `npm --version`
 echo "node.js : " `node --version | awk '{print substr($0,2)}'`
 echo "git     : " `git --version  | awk -F ' ' '{print $3}'`
 
-mkdir -p ${WORKDIR}/source
-if [ ! -d "${WORKDIR}/source/${GITHUB_REPO}" ]; then
-  git clone https://github.com/${GITHUB_USER}/${GITHUB_REPO}.git ${WORKDIR}/source/${GITHUB_REPO}
+mkdir -p ${SOURCE_DIR}
+if [ ! -d "${SOURCE_DIR}/${GITHUB_USER}/${GITHUB_REPO}" ]; then
+  git clone https://github.com/${GITHUB_USER}/${GITHUB_REPO}.git ${SOURCE_DIR}/${GITHUB_USER}/${GITHUB_REPO}
 fi
 
-cd ${WORKDIR}/source/${GITHUB_REPO}
+cd ${SOURCE_DIR}/${GITHUB_USER}/${GITHUB_REPO}
 npm install
 hexo generate
 
-cd ${WORKDIR}/source/${GITHUB_REPO}/public
+BUILD_DEST=${BUILDS_DIR}/${BUILD_NO}
+mkdir -p ${BUILD_DEST}
+
+cd ${SOURCE_DIR}/${GITHUB_USER}/${GITHUB_REPO}/public
 tar -cvzf ${BUILD_DEST}/${GITHUB_REPO}.tar.gz *
+
 cd ${BUILD_DEST}
 sha256sum ${GITHUB_REPO}.tar.gz > ${GITHUB_REPO}.sha256
+
+cd ${SOURCE_DIR}/${GITHUB_USER}/${GITHUB_REPO}/public
+mkdir -p ${BUILD_DEST}/${GITHUB_REPO}
+cp -r ${SOURCE_DIR}/${GITHUB_USER}/${GITHUB_REPO}/public/* ${BUILD_DEST}/${GITHUB_REPO}/
--- build/deploy.sh
+++ build/deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+if [ -z "${WORKDIR}" ]; then
+  echo "ABORT: env WORKDIR is missing"
+  exit 1
+fi
+
 if [ -z "${GITHUB_USER}" ]; then
   echo "ABORT: env GITHUB_USER is missing"
   exit 1
@@ -21,22 +26,25 @@ if [ -z "${BUILD_NO}" ]; then
   exit 1
 fi
 
-if [ -z "${TRAVIS_BUILD_NUMBER}" ]; then
-  echo "ABORT: env TRAVIS_BUILD_NUMBER is missing"
-  exit 1
+WORKDIR=${WORKDIR:-/tmp}
+BUILDS_DIR=${WORKDIR}/builds
+BUILD_DEST=${BUILDS_DIR}/${BUILD_NO}
+
+if [ -z "${TRAVIS_BUILD_NUMBER}" ] || [ -z "${TRAVIS_REPO_SLUG}" ] || [ -z "${TRAVIS_BUILD_ID}" ] ; then
+  GITHUB_RELTEXT="Manually-released"
+else
+  GITHUB_RELTEXT="Auto-released by [Travis-CI build #${TRAVIS_BUILD_NUMBER}](https://travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID})"
 fi
 
-if [ -z "${TRAVIS_REPO_SLUG}" ]; then
-  echo "ABORT: env TRAVIS_REPO_SLUG is missing"
-  exit 1
-fi
+cd ${BUILD_DEST}/${GITHUB_REPO}
 
-if [ -z "${TRAVIS_BUILD_ID}" ]; then
-  echo "ABORT: env TRAVIS_BUILD_ID is missing"
-  exit 1
-fi
-
-GITHUB_RELTEXT="Auto-released by [Travis-CI build #${TRAVIS_BUILD_NUMBER}](https://travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID})"
+git init
+git config user.email "---"
+git config user.name "---"
+git add --all
+git commit -m "${GITHUB_RELTEXT}"
+git remote add origin https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_USER}/${GITHUB_USER}.github.io.git
+git push -u origin master --force
 
 ghr --version
 ghr --debug -u "${GITHUB_USER}" -r "${GITHUB_REPO}" -b "${GITHUB_RELTEXT}" "v${BUILD_NO}" /builds/${BUILD_NO}/
